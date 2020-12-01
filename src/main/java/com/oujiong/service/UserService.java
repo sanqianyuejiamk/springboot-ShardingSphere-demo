4c8bbb3a5ccb689bab5930eb97a7d86c26e07e2f
934734735@qq.com <934734735@qq.com>
2020-12-01 09:50:17 +0800

    springboot-ShardingSphere-demo

---
+++ src/main/java/com/oujiong/service/UserService.java
@@ -0,0 +1,21 @@
+package com.oujiong.service;
+
+import com.oujiong.entity.User;
+
+import java.util.List;
+
+
+public interface UserService {
+
+    /**
+     * 获取所有用户信息
+     */
+    List<User>  list();
+
+    /**
+     *   批量 保存用户信息
+     * @param userVOList
+     */
+    String  insertForeach(List<User> userVOList);
+
+}
